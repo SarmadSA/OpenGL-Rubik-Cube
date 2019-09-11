8630420f6bf6aac589495e897e275673bf105e1c
sarmadsa <sarmad@virusoftware.com>
2019-09-11 22:28:17 +0200

    Add properties and implement classes

--- src/RubicsCubeRenderer.java
+++ src/RubicsCubeRenderer.java
@@ -1,3 +1,42 @@
-public class RubicsCubeRenderer {
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.glu.GLU;
 
+
+public class RubicsCubeRenderer extends GLCanvas implements GLEventListener {
+
+    //Properties
+    private static final String TITILE = "Rubik's Cube";
+    private static final int CANVAS_WIDTH  = 640;
+    private static final int CANVAS_HEIGHT = 480;
+    private static final int FPS = 60;
+
+    private GLU glu;
+
+
+    //Constructor
+    public RubicsCubeRenderer(){
+
+    }
+
+    @Override
+    public void init(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable drawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+
+    }
 }
